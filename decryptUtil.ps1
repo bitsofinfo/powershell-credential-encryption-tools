c2cf8329ce6c3f82b1ccecd5d4e2b0256e5894af
bitsofinfo <bitsofinfo.g@gmail.com>
2015-01-16 05:57:26 +0000

    added decrypt to PSCredential

--- decryptUtil.ps1
+++ decryptUtil.ps1
@@ -3,6 +3,9 @@ function secureString2Cleartext($secureString) {
     $bstr = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)
     $clear = [Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
     [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($bstr) # zero memory
+
+    Remove-Variable bstr
+
     return $clear
 }
 
@@ -23,11 +26,44 @@ function decrypt2SecureString($jsonCredentialFile, $b64KeyFile, $property) {
     $keyAsSecureString = ConvertTo-SecureString -String $keyBytesUnicode -AsPlainText -Force
 
     # decrypt to a SecureStrings
-    return ConvertTo-SecureString -String $propertyEncrypted -SecureKey $keyAsSecureString
+    $secureString = ConvertTo-SecureString -String $propertyEncrypted -SecureKey $keyAsSecureString
+
+
+    Remove-Variable keyAsB64
+    Remove-Variable jsonCredentials
+    Remove-Variable credentialObj
+    Remove-Variable propertyEncrypted
+    Remove-Variable rawKeyBytes
+    Remove-Variable keyBytesUnicode
+    Remove-Variable keyAsSecureString
+
+    return $secureString
 
 }
+
+# Decrypt the credentials from the jsonCredentialFile using b64KeyFile
+# all file variables are expected to be legit paths to the respective files
+# This *assumes* that 2 properties exist in the JSON file that are encrypted
+# that of 'username' and 'password'. This will return a usable PSCredential
+# object that can be used
+function decrypt2PSCredential($jsonCredentialFile, $b64KeyFile) {
+
+    $passwordSecureString = decrypt2SecureString $jsonCredentialFile $b64KeyFile 'password'
+    $usernameSecureString = decrypt2SecureString $jsonCredentialFile $b64KeyFile 'username'
+    $usernameClearText = secureString2Cleartext $usernameSecureString
+    $psCredential = new-object -typename System.Management.Automation.PSCredential -argumentlist $usernameClearText,$passwordSecureString
+
+    Remove-Variable passwordSecureString
+    Remove-Variable usernameSecureString
+    Remove-Variable usernameClearText
+
+    return $psCredential
+}
+
+
 
 # For testing...
 #$asSecureString = decrypt2SecureString 'encrypted.credentials' 'secret.key' 'password'
 #$asCleartext = secureString2ClearText $asSecureString
 #Write-Output $asCleartext
+#Write-Output (decrypt2PSCredential 'encrypted.credentials' 'secret.key')
